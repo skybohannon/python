a9ea2322b515f05b8bdea9f6c111338e8b3980bd
Sky Bohannon <skybohannon@gmail.com>
2017-10-09 22:06:52 -0500

    Tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,21 @@
+# t = "a", "b", "c", "d",
+# print(t)
+#
+# print("a", "b", "c", "d")
+# print(("a", "b", "c", "d"))
+
+welcome = "Welcome to my nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Imilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
+
+
+metallica2 = ["Ride the Lightning", "Metallica", 1984]
+print(metallica2)
+
+#unpacking the tuple
+title, artist, year = imelda
+print(title)
+print(artist)
+print(year)

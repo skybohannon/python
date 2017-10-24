002d19ffc1aaf6f64d809a80b3cd0a54beda8194
Sky Bohannon <skybohannon@gmail.com>
2017-10-24 13:50:37 -0500

    ooouuu

--- RandomProjects/primality.py
+++ RandomProjects/primality.py
@@ -1,18 +1,21 @@
 def get_number():
     return int(input("Please enter a number: "))
 
+try:
+    userNumber = get_number()
 
-userNumber = get_number()
+    userNumberRange = list(range(1, userNumber+1))
+    divisors = []
 
-userNumberRange = list(range(1, userNumber+1))
-divisors = []
+    for num in userNumberRange:
+        if userNumber % num == 0:
+            divisors.append(num)
 
-for num in userNumberRange:
-    if userNumber % num == 0:
-        divisors.append(num)
+    if len(divisors) == 2:
+        print("{} was a prime number!".format(userNumber))
+    else:
+        divisorsString = ', '.join(str(x) for x in divisors)
+        print("{} wasn't prime. It's divisors are {}".format(userNumber, divisorsString))
 
-if len(divisors) == 2:
-    print("{} was a prime number!".format(userNumber))
-else:
-    divisorsString = ', '.join(str(x) for x in divisors)
-    print("{} wasn't prime. It's divisors are {}".format(userNumber, divisorsString))
+except ValueError:
+    print("You did not enter a number. You had one job.")
